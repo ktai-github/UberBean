086159523585dfc60c417c64d0b7a26b028b55f7
Kevin T <kevin.tai.mail@gmail.com>
2018-03-02 14:44:58 -0800

    initial commit

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by KevinT on 2018-03-02.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberBean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberBean
+//
+//  Created by KevinT on 2018-03-02.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
